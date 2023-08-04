66089ccb7f90362905babd5e361b059342d1fd3f
Martijn van der Blom <martijn.vanderblom@pinkroccade.nl>
2023-08-04 13:47:39 +0200

    Puts the DatabasePopulator behind a dev profile and adds a runconfig for local development

--- inventory-demo-backend/src/main/kotlin/dev/vanderblom/inventorybackenddemo/data/DatabasePopulator.kt
+++ inventory-demo-backend/src/main/kotlin/dev/vanderblom/inventorybackenddemo/data/DatabasePopulator.kt
@@ -2,9 +2,11 @@ package dev.vanderblom.inventorybackenddemo.data
 
 import dev.vanderblom.inventorybackenddemo.data.model.Product
 import jakarta.annotation.PostConstruct
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
+@Profile("dev")
 class DatabasePopulator(
     private val repo: ProductRepository
 ) {
